61b396941a64981e45f06cafd8335ff42998a10d
ashish <148787104+ashish3410@users.noreply.github.com>
2024-09-05 15:21:16 +0000

    type conversion

---
+++ 01_Basics/TypeConversion.js
@@ -0,0 +1,36 @@
+let num=null
+let StringNum=String(num)
+// console.log(StringNum);
+// console.log(typeof StringNum);
+
+// 33 => "33"
+//  true="true"
+//  null => " null"
+
+let stringVar=null
+let booleanNum=Boolean(stringVar)
+console.log(booleanNum);
+console.log(typeof booleanNum);
+
+// "" => False
+//  "Ashish" => True
+//  null= False
+
+
+let string=null
+let number=Number(string);
+// console.log(number);
+// console.log(typeof number);
+
+// "" => 0
+// "Ashish" => NaN(Not a Number)
+//  true => 1 and false => 0
+//  null => 0
+
+
+
+// Operations
+
+// x++ => increment the value and return the value before updation
+
+// ++x => increament the value and return the value after updation
