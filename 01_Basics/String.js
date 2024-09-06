302b4572b1fad1c1764933f9b375c3fb8d8a13bb
ashish <148787104+ashish3410@users.noreply.github.com>
2024-09-06 14:53:04 +0000

    String Manipulation

---
+++ 01_Basics/String.js
@@ -0,0 +1,20 @@
+let name="Ashish"
+let repoCount=35
+console.log(name)
+console.log(`My name is ${name} and my repo count is ${repoCount}`)
+
+// String Declaration
+let city = new String("Lucknow_is_my_Home_City")
+
+// String Method
+console.log(name.trim())
+console.log(name.toUpperCase())
+console.log(city.substring(0,4))
+console.log(city.charAt(4))
+console.log(city.indexOf("k"))
+console.log(city.slice(3,-8)) //takes negative values as well
+console.log(city.includes("is"));
+console.log(city.split("_"));
+
+
+
