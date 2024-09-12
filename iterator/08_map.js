c56e56cd3908732c209f79a9b3e5d9eb4a4065c5
ashish <148787104+ashish3410@users.noreply.github.com>
2024-09-12 04:57:29 +0000

    iterators

---
+++ iterator/08_map.js
@@ -0,0 +1,14 @@
+let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let newArr=arr.map( (num)=> num+10)
+
+
+// this is called as channing
+// let newArr=arr.map( (num)=> num*10).map( (num)=> num+1).filter( (num)=> num>41)
+
+
+// Square of items in array
+let newArr=arr.map( (num)=> num*num)
+console.log(newArr)
+
+
+
