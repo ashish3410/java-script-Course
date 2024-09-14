1171d21141e72c942f0a60265af62130f2dc4d5a
ashish <148787104+ashish3410@users.noreply.github.com>
2024-09-14 20:30:38 +0530

    Update script.js

--- project04/script.js
+++ project04/script.js
@@ -1,54 +1,67 @@
-const ytLink=document.querySelector(".youTubeLink")
-const guess=document.querySelector("#submitGuess")
-const result=document.querySelector(".loWhigh")
-const userInput=parseInt(document.getElementById("guessField").value)
-const prev=document.querySelector(".previous")
-const remain=document.querySelector(".remaining")
+const ytLink = document.querySelector(".youTubeLink")
+const guess = document.querySelector("#submitGuess")
+let result = document.querySelector(".lowHigh")
+const prev = document.querySelector(".previous")
+const remain = document.querySelector(".remaining")
+const newGameButton = document.querySelector(".newGame")
+let userInput;
 
-
-ytLink.addEventListener('click',GoToYT)
-function GoToYT(){
-    location.href="https://www.youtube.com/watch?v=EGqHVjU-fas&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=35"
+ytLink.addEventListener('click', GoToYT)
+function GoToYT() {
+    location.href = "https://www.youtube.com/watch?v=EGqHVjU-fas&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=35"
 }
-let prevGuesses=0;
-let remaining=10;
-let playGame=true;
-const randomNum=Math.floor((Math.random()*100)+1)
+let prevGuesses = 0;
+let remaining = 10;
+const randomNum = Math.floor((Math.random() * 100) + 1)
 
-guess.addEventListener("submit",()=>{
+guess.addEventListener("click", (event) => {
+    event.preventDefault()
+    userInput = parseInt(document.getElementById("guessField").value)
     validateGame()
     calChances()
     reduceChances()
     prevGuess()
-    if(userInput<randomNum){
-        result.innerHTML=`<h4> Your guess ${userInput} is low </h4>`
-    }else if(userInput.val>randomNum){
-        result.innerHTML=`<h4> Your guess ${userInput} is high </h4>`
-    }else if(userInput.val===randomNum){
-        result.innerHTML=`<h4> Hurray! You smash it </h4>`
-    }
 })
 
 
-function validateGame(){
-    if(userInput=="" || userInput<0 || userInput>100 || isNaN(userInput)){
-        result.innerHTML="Please Enter the valid input (Number Between 1 to 100)"
+function validateGame() {
+    if (userInput < 0 || userInput > 100 || isNaN(userInput)) {
+        result.innerHTML = "<h4>Please Enter the valid input (Number Between 1 to 100)</h4>"
+        remaining = remaining + 1;
+        remain.innerHTML = `Remaining Guesses: ${remaining}`
+        return
+    } else {
+        if (userInput < randomNum) {
+            result.innerHTML = `<h4> Your guess ${userInput} is low </h4>`
+        } else if (userInput > randomNum) {
+            result.innerHTML = `<h4> Your guess ${userInput} is high </h4>`
+        } else if (userInput === randomNum) {
+            result.innerHTML = `<h4> Hurray! You smash it </h4>`
+        }
     }
-} 
+}
 
-function calChances(){
-    if(remaining===0){
-        guess.ariaDisabled=false
+function calChances() {
+    if (remaining <= 0) {
+        result.innerHTML = "<h4>Game Over! You've used all your guesses.</h4>";
+        guess.disabled() = false
     }
 }
 
-function reduceChances(){
-    remaining=remaining-1;
-    remain.innerHTML=`Remaining Guesses: ${remaining}`
+function reduceChances() {
+    remaining = remaining - 1;
+    remain.innerHTML = `Remaining Guesses: ${remaining}`
 
 }
-function prevGuess(){
-    prevGuesses=guess
-    prev.innerHTML=`Previous Guess ${prevGuess}`
+function prevGuess() {
+    prevGuesses = userInput
+    prev.innerHTML = `Previous Guess ${prevGuesses}`
 }
+newGameButton.addEventListener("click", () => {
+    prev.innerHTML = `Previous Guess`
+    remain.innerHTML = `Remaining Guesses: 10`
+    result.style.display="none"
+    userInput=""
+    
 
+})
